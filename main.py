6e7e54f39fc225225a1cdcfd6b44fef815f8775e
Carolyn Ryan <ryanc2@carleton.edu>
2017-10-17 10:33:54 -0500

    Initial recipe list and reader

---
+++ main.py
@@ -0,0 +1,23 @@
+from recipe_scrapers import scrap_me as scrape
+# https://github.com/hhursev/recipe-scraper
+
+def scrape_recipes():
+    recipe_urls = open("cake_recipe_urls.txt", "r")
+    recipe_csv = open("cake_recipe.csv", "w")
+    recipe_csv.write("Title, Time, Ingredients, Instructions\n")
+    for url in recipe_urls.readlines():
+        scrape_recipe = scrape(url)
+        title = scrape_recipe.title().replace(",", " ")
+        ingredients = ";".join(scrape_recipe.ingredients()).replace(",", " ")
+        instructions = scrape_recipe.instructions().replace(",", " ").replace("\n", "  ")
+        recipe_csv.write("{}, {}, {}, {}\n".format(
+            title, scrape_recipe.total_time(), ingredients, instructions))
+
+        # print("Title: {}, Time: {}\nIngredients: {}\nInstructions: {}\n".format(
+        #     scrape_recipe.title(), scrape_recipe.total_time(), scrape_recipe.ingredients(), scrape_recipe.instructions()))
+
+    recipe_urls.close()
+    recipe_csv.close()
+
+if __name__ == '__main__':
+    scrape_recipes()
