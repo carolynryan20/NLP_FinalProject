f92696ba3db8dd361a254bc7053147aa2481e0d2
Carolyn Ryan <ryanc2@carleton.edu>
2017-11-04 11:56:30 -0500

    Basic generator structure

--- recipe_generator.py
+++ recipe_generator.py
@@ -6,6 +6,20 @@ def main():
     # can get all columns with recipes_df.title, recipes_df.time, recipes_df.ingredients, recipes_df.instructions
 
     print(recipes_df)
-    
+
+def get_ingredient_list():
+    user_input_ingredients = []
+    ingredient = input("Enter an ingredient.  Hit q when done entering ingredients. ")
+    while ingredient != 'q' and ingredient:
+        user_input_ingredients.append(ingredient)
+        ingredient = input("Enter an ingredient.  Hit q when done entering ingredients. ")
+
+    print(user_input_ingredients)
+
+    # Choose ingredient amounts
+    # Select more ingredients if need be
+    # Return complete ingredients
+
 if __name__ == '__main__':
-    main()
+    # main()
+    get_ingredient_list()
