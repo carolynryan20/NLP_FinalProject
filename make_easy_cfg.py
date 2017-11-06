d109d3324c8dc1f980fa35d7861e2fc4835da5e1
Carolyn Ryan <ryanc2@carleton.edu>
2017-11-06 16:46:17 -0600

    Make easy cfg

--- make_easy_cfg.py
+++ make_easy_cfg.py
@@ -1,25 +1,76 @@
 from pandas import read_csv
 import re
+from nltk import pos_tag
+from nltk.grammar import CFG
+from nltk.parse.generate import generate
 
-def main():
+EXCLUDE = ":;(),.'\""
+def construct_cfg():
     cols = ['title', 'ingredients', 'instructions']
     recipes_df = read_csv("recipes.csv", names=cols, encoding='latin-1')
 
-    preheat_oven_to = re.compile('\s*(\d|/|\\\|\s)+')
+    # preheat_oven_to = re.compile('\s*(\d|/|\\\|\s)+')
     temp_list = []
+    verb_list = []
+    the_rest_list = []
     for recipe in recipes_df['instructions']:
         # print(recipe)
         instructions = recipe.split(".")
         if 'reheat oven to ' in instructions[0]:
-            idc, temp = instructions[0].split('reheat oven to ')
-            temp_list.append(temp)
+            temp = instructions[0].split('reheat oven to ')[1]
+            temp = ''.join(ch for ch in temp if ch not in EXCLUDE)
+            if temp not in temp_list and len(temp) > 0:
+                temp_list.append(temp)
             instructions.pop(0)
         for instruction in instructions:
-            # TODO could add tags
+            # TODO could add POS tags
+            instruction_list = instruction.split()
+            pos = pos_tag(instruction_list)
+            # print(pos, len(pos))
 
+            if len(pos) > 1 and ('V' in pos[0][1] or 'NNP' in pos[0][1]):
+                verb = ''.join(ch for ch in pos[0][0] if ch not in EXCLUDE)
+                if verb not in verb_list and len(verb) > 0:
+                    verb_list.append(verb)
 
     print(temp_list)
+    print(verb_list)
+    # print(the_rest_list)
 
+    easy_cfg = '''
+S -> P INS
+P -> PTOT TEMP PERIOD
+PTOT -> 'Preheat the oven to'
+
+INS -> V DET INGR PERIOD | INS INS
+DET -> 'the'
+INGR -> 'eggs' | 'flour' | 'sugar'
+PERIOD -> '.'
+'''
+
+    verb_rule = "V -> "
+    for verb in verb_list:
+        # easy_cfg += "\nV -> '{}'".format(verb)
+        verb_rule += " '{}' |".format(verb)
+
+    temp_rule = "TEMP -> "
+    for temp in temp_list:
+        # easy_cfg += "\nTEMP -> '{}'".format(temp)
+        temp_rule += " '{}' |".format(temp)
+
+    easy_cfg += verb_rule[:-1] + "\n"
+    easy_cfg += temp_rule[:-1] + "\n"
+
+    print(easy_cfg)
+    with open("easy_cfg.txt", "w") as cfg_file:
+        cfg_file.write(easy_cfg)
 
 if __name__ == '__main__':
-    main()
+    # construct_cfg()
+
+    with open("easy_cfg.txt", "r") as cfg_file:
+        easy_cfg = cfg_file.read()
+    my_easy_cfg = CFG.fromstring(easy_cfg)
+    print(easy_cfg)
+    for sentence in generate(my_easy_cfg, depth = 4):
+        print(' '.join(sentence))
